2105bac7d566ee36195626e78c64a33e7ecd085e
LiQC(李庆超/深圳) <liqc@paxsz.local>
2017-09-04 16:23:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/liqc/uiwidgettest/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.liqc.uiwidgettest;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button_1);
+        progressBar = (ProgressBar)findViewById(R.id.progress_bar);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_1:
+//                if(progressBar.getVisibility() == View.GONE){
+//                    progressBar.setVisibility(View.VISIBLE);
+//                }else {
+//                    progressBar.setVisibility(View.GONE);
+//                }
+                int progress = progressBar.getProgress();
+                progress = progress + 10;
+                progressBar.setProgress(progress);
+                break;
+            default:
+                break;
+        }
+    }
+}
